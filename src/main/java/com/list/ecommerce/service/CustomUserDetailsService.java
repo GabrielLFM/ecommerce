ce990023e365e2d1d3ca2bc495ee962a5ad9b22a
luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-04-14 22:05:34 -0300

    feat: add config service

--- src/main/java/com/list/ecommerce/service/CustomUserDetailsService.java
+++ src/main/java/com/list/ecommerce/service/CustomUserDetailsService.java
@@ -1,4 +1,30 @@
 package com.list.ecommerce.service;
 
-public class CustomUserDetailsService {
+import com.list.ecommerce.entity.Usuario;
+import com.list.ecommerce.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public CustomUserDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(email).orElseThrow(()-> new RuntimeException("Usuario não encontrado"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(usuario.getEmail())
+                .password(usuario.getSenha())
+                .roles(usuario.getRoles().name().replace("ROLE_",""))
+                .build();
+    }
 }
+
