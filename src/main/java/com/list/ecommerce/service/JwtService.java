08fa01b58b3328815b280b221c90bc330240a3e0
GabrielLFM <biel.lucianomelo@gmail.com>
2026-05-20 10:21:59 -0300

    fix: add jwt on ecommerce api

---
+++ src/main/java/com/list/ecommerce/service/JwtService.java
@@ -0,0 +1,27 @@
+package com.list.ecommerce.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    private final String SECRET = "minhamaemandoeuescolheressdfsdfdfdfdfdsfdsfsadaaquimascomoeusouteimoso";
+
+    public String gerarToken(String email){
+        return Jwts.builder()
+                .setSubject(email)
+                .signWith(SignatureAlgorithm.HS256,SECRET)
+                .compact();
+
+    }
+
+    public String pegarEmail(String token){
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
